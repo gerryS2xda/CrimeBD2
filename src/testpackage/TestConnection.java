d39405a8adbbe8e46b2cfdbb3f2f74da5d0a9406
Gennaro Teodoro <#Casatiello233>
2020-06-26 01:27:34 +0200

    Testing connection with neo4j

---
+++ src/testpackage/TestConnection.java
@@ -0,0 +1,35 @@
+package testpackage;
+
+import org.neo4j.driver.*;
+
+import static org.neo4j.driver.Values.parameters;
+
+public class TestConnection implements AutoCloseable {
+
+    private final Driver driver;
+
+    public TestConnection(String uri, String username, String password){
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(username,password));
+    }
+
+    @Override
+    public void close() throws Exception {
+        driver.close();
+    }
+
+    public void insert(){
+        try ( Session session = driver.session() ) {
+
+
+            session.writeTransaction(tx -> tx.run("MERGE (c:crime { incident_number: $x }",parameters("x",1)));
+        }
+    }
+
+    public static void main(String[] args){
+
+        TestConnection tc= new TestConnection("bolt://localhost:7687","neo4j","root");
+        tc.insert();
+    }
+
+
+}
